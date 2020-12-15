1a20b39df2216e3fd8752ffc1f79cdb753b82fbf
Sopyan7 <63286614+Sopyan7@users.noreply.github.com>
2020-12-15 13:48:56 +0200

    Add files via upload

--- gradingsystem.py
+++ gradingsystem.py
@@ -1,4 +1,7 @@
-# Bubble Sort Algorithm
+import json
+from os import system, name
+from time import sleep
+
 def sort_algorithm(list, key=lambda obj: obj):
     changed = True
     while changed:
@@ -66,15 +69,54 @@ def table_print(headers, rows):
             item_str += str(item).ljust(longest[itemno]) + ' '
         print(item_str[:-1])
 
-#Entering Module (name, code, N of assessments)
+def save_data_module(data:dict):
+    try:
+        with open('module.json','r') as file:
+            json_data = json.load(file)
+            json_data['modules'].append(data)
+            with open('module.json','w') as file1:
+                json.dump(json_data,file1)
+    except Exception as E:
+        print(E)
+        json_data = {
+            'modules':[]
+        }
+        json_data['modules'].append(data)
+        with open('module.json','w') as file1:
+            json.dump(json_data,file1)
+
+def save_student(data:dict , id:str):
+    try:
+        with open('module.json','r') as file:
+                json_data = json.load(file)
+                for module in json_data['modules']:
+                    if module["id"] == id:
+                        module["students"].append(data)
+                        with open('module.json','w') as file1:
+                            json.dump(json_data,file1)
+                            return True
+    except:
+        return False
+
+def get_module_by_id(id:str):
+    with open('module.json','r') as file:
+        json_data = json.load(file)
+        for module in json_data['modules']:
+            if module["id"] == id:
+                return module
+        return False
+
+
 class Module:
-    def __init__(self, name, code, assessments, weights):
+
+    def __init__(self,id,name,code,assessments,weights,students=[]):
+        self.id = id
         self.name = name
         self.code = code
-        self.asssessments = assessments
+        self.assessments = assessments
         self.weights = weights
-        self.students = []
-
+        self.students = students
+    
     def calculate_average_score(self, assessment=None):
         if self.students:
             scores = []
@@ -103,15 +145,13 @@ class Module:
 
     def display_student_table(self, sort='score', full_student_info=False):
         if sort == 'firstname':
-            def key(student: Student):
+            def key(student):
                 return student.firstname
         elif sort == 'lastname':
-            def key(student: Student):
+            def key(student):
                 return student.lastname
         elif sort == 'score':
-            def key(student: Student):
-                # I could use `student.average`, but the task strictly says "Sort the output based on the TOTAL SCORE [...]"
-                # I invert the value, because the best score should be at the top
+            def key(student):
                 return -sum(student.scores)
         else:
             raise ValueError("sort must be 'firstname', 'lastname' or 'score'")
@@ -126,9 +166,8 @@ class Module:
 
         rows = []
         for student in students:
-            row = [student.firstname, student.lastname, 0,
-                   student.average]
-            for x in range(self.asssessments):
+            row = [student.firstname, student.lastname, 0,student.average]
+            for x in range(self.assessments):
                 row[2] += student.scores[x] * self.weights[x]
             row[2] = int(row[2])
             row.extend(performance(row[2]))
@@ -144,12 +183,11 @@ class Module:
         rows = [
             ['All combined', *self.get_min_max_avg()]
         ]
-        for assessment in range(self.asssessments):
+        for assessment in range(self.assessments):
             rows.append([assessment, *self.get_min_max_avg(assessment)])
 
         table_print(headers, rows)
 
-#Students Class
 class Student:
     def __init__(self, firstname, lastname, id, scores):
         self.firstname = firstname
@@ -178,6 +216,26 @@ class InteractiveControl:
     def __init__(self):
         self.module = None
 
+    def object_decoder(self,data:dict):
+        print(data)
+        self.module.id = data["id"]
+        self.module.name = data["name"]
+        self.module.code = data["code"]
+        self.module.assessments = data["assessments"]
+        self.module.weights = data["weights"]
+        self.module.students = data["students"]
+    
+    def show_objects(self):
+        for data in range(self.module):
+            print(data.__dict__)
+    
+    def show_data(self):
+        print('Module ID - ' + self.module.id)
+        print('Module Name - ' + self.module.name)
+        print('Module Code - ' + self.module.code)
+        print('Assessments - ' + str(self.module.assessments))
+        print('Weights - ' + str(self.module.weights))
+
     def typed_input(self, phrase, type, typename):
         while True:
             str = input(phrase)
@@ -185,17 +243,8 @@ class InteractiveControl:
                 return type(str)
             except ValueError:
                 print('Expected a ' + typename)
-		#Creating Module
-    def create_module(self):
-        print('--[Module creation]--')
-        name = input('Module name: ')
-        code = input('Module code: ')
-        while True:
-            assessments = self.typed_input('Amount of assessments: ', int,
-                                           'whole number')
-            if assessments > 0:
-                break
-            print('Amount must be above 0.')
+    
+    def weights_creation(self,assessments):
         weights = []
         while len(weights) < assessments:
             while True:
@@ -208,33 +257,65 @@ class InteractiveControl:
                     break
                 print('Weight must be between 0 and 1.')
             weights.append(weight)
+        return weights
 
-        self.module = Module(name, code, assessments, weights)
-        print('√ Created the module ' + name)
-		
-		#Adding Student
+	#Creating Module
+    def create_module(self):
+        print('--[Module creation]--')
+        id = input('Module ID: ')
+        name = input('Module name: ')
+        code = input('Module code: ')
+        while True:
+            assessments = self.typed_input('Amount of assessments: ', int,'whole number')
+            if assessments > 0:
+                break
+            print('Amount must be above 0.')
+        
+        while True:
+            weights = self.weights_creation(assessments)
+            sum = 0
+            for i in weights:
+                sum = sum + i
+            
+            if sum == 1.0:
+                break
+            print('All Weights should be add upto 1.0')
+
+        self.module = Module(id , name, code, assessments, weights)
+        save_data_module(self.module.__dict__)
+        print('√ Created the module ' + name + ' With ID ' + id)
+
     def add_student(self):
-        print('--[Add student]--')
-        firstname = input('Firstname of the student: ')
-        lastname = input('Lastname of the student: ')
-        studentid = self.typed_input('Student ID: ', int, 'whole number')
-        scores = []
-        while len(scores) < self.module.asssessments:
-            while True:
-                score = self.typed_input(
-                    'Points from assessment ' + str(len(scores) + 1) + ': ',
-                    int,
-                    'whole number'
-                )
-                if 100 >= score >= 0:
-                    break
-                print('Score must be between 0 and 100.')
-            scores.append(score)
+        module_id = input('Please pass the module ID:- ')
+        module = get_module_by_id(module_id)
+        if module:
+            self.module = Module(module["id"],module["name"],module["code"],module["assessments"],module["weights"],module["students"])
+            clear()
+            print('--[Add student]--')
+            firstname = input('Firstname of the student: ')
+            lastname = input('Lastname of the student: ')
+            studentid = self.typed_input('Student ID: ', int, 'whole number')
+            scores = []
+            while len(scores) < self.module.assessments:
+                while True:
+                    score = self.typed_input(
+                        'Points from assessment ' + str(len(scores) + 1) + ': ',
+                        int,
+                        'whole number'
+                    )
+                    if 100 >= score >= 0:
+                        break
+                    print('Score must be between 0 and 100.')
+                scores.append(score)
 
-        student = Student(firstname, lastname, studentid, scores)
-        self.module.students.append(student)
-        print('√ Added student ' + firstname + ' ' + lastname)
-		#Listing Options
+            student = Student(firstname, lastname, studentid, scores)
+            self.module.students.append(student)
+            save_student(student.__dict__,module_id)
+            print('√ Added student ' + firstname + ' ' + lastname)
+        else:
+            print('Sorry we couldn\'t find that. Please recheck and try again.')
+            sleep(2)
+    
     def list(self):
         print('--[List Settings]--')
         while True:
@@ -246,31 +327,92 @@ class InteractiveControl:
             sortmode = input('Sorted by? [firstname/lastname/score] ')
             if sortmode in ('firstname', 'lastname', 'score'):
                 break
-
         self.module.display_student_table(sortmode, yesno)
-		#Choosing Options
-    def main(self):
-        self.create_module()
-        while True:
-            print('--[Main Menu]--')
-            print('Actions:')
-            print('  1. Add student')
-            print('  2. List students')
-            print('  3. List lowest/highest score of assessments')
-            action = self.typed_input('Action: ', int, 'whole number')
-            if action == 1:
-                self.add_student()
-            elif action == 2:
-                self.list()
-                input('Press return to continue> ')
-            elif action == 3:
-                self.module.display_assessments()
-                input('Press return to continue> ')
 
-            else:
-                print('Invalid action.')
+def main_menu():
+    print('--[Main Menu]--')
+    print('Actions:')
+    print('1. Add a new Module')
+    print('2. Add new Modules ( More than 1)')
+    print('3. Move to Student Menu')
+    print('4. Exit')
 
-#Main Class
+def action_1(ic):
+    ic.create_module()
+    sleep(2)
+
+def action_2(ic):
+    modules = None
+    while True:
+        try:
+            modules = int(input('How many modules you want to create - '))
+            break
+        except ValueError:
+            print('Value should be integer')
+    count = 0
+    while count < modules:
+        count = count + 1
+        ic.create_module()
+    sleep(2)
+    system('cls')
+
+def main_function(ic):
+    while True:
+        system('cls')
+        main_menu()
+        select = int(input('Enter your Choice: '))
+        clear()
+        if select < 1 or select > 4:
+            print('Invalid Choice.')
+            sleep(2)
+            clear()
+        elif select == 1:
+            action_1(ic)
+        elif select == 2:
+            action_2(ic)
+        elif select == 3:
+            clear()
+            student_function(ic)
+            clear()
+        elif select == 4:
+            print('See you soon :)')
+            exit(0)
+
+def student_function(ic):
+    while True:
+        print('--[Student Menu]--')
+        print('Actions:')
+        print('  1. Add student')
+        print('  2. List students')
+        print('  3. List lowest/highest score and weights of assessments')
+        print('  4. Go Back to main menu')
+        select = int(input('Enter your Choice: '))
+        clear()
+        if select < 1 or select > 4:
+            print('Invalid Choice.')
+            exit(0)
+        elif select == 1:
+            ic.add_student()
+            sleep(2)
+            clear()
+        elif select == 2:
+            ic.list()
+            input('Press return to continue> ')
+            clear()
+        elif select == 3:
+            ic.module.display_assessments()
+            input('Press return to continue> ')
+            clear()
+        elif select == 4:
+            break
+
+
+def clear():
+    system('cls')
+
 if __name__ == '__main__':
     ic = InteractiveControl()
-    ic.main()
+    while True:
+        main_function(ic)
+        clear()
+
